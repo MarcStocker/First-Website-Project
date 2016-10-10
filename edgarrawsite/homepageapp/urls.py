58960d273360f3a36a80bddf3061e00a28f6bcfe
MarcStocker <mstocker.gbit@gmail.com>
2016-10-10 02:21:34 -0700

    Added website Project Folder - Setup with basic Homepage app

---
+++ edgarrawsite/homepageapp/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    #<WebSite.com>
+    url(r'^$', views.index, name="index"),
+    #<WebSite.com>/home/
+    url(r'^home/$', views.index, name="index"),
+]
