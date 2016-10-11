535482e8e53a1c15cff769570bee0f8fc1ec2002
MarcStocker <mstocker.gbit@gmail.com>
2016-10-11 09:56:01 -0700

    Added todo list - extends base.html implemented

--- edgarrawsite/homepageapp/models.py
+++ edgarrawsite/homepageapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class TodoList(models.Model):
+    item = models.CharField(max_length=144)
+    is_done = models.BooleanField(default=False)
+    in_progress = models.BooleanField(default=False)
+    # check = models.ImageField(upload_to='media/checkmark image.jpg')
+
+    def __str__(self):
+        if self.is_done == True:
+            return 'Completed: ' + self.item
+        elif self.in_progress == True:
+            return 'In progress: ' + self.item
+        else:
+            return 'Not started: ' + self.item
+    class Meta:
+        ordering = ["in_progress"]
--- edgarrawsite/homepageapp/urls.py
+++ edgarrawsite/homepageapp/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls import url
 from . import views
-
 urlpatterns = [
     #<WebSite.com>
     url(r'^$', views.index, name="index"),
